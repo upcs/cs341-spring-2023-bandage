0548a606336804bef02093266f1d95e4972408f8
millerl23 <112990842+millerl23@users.noreply.github.com>
2023-02-28 19:45:53 -0800

    Refined Donate snapshot test

--- diagnosis/__tests__/Donate.test.js
+++ diagnosis/__tests__/Donate.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {fireEvent, render} from '@testing-library/react-native';
 import Donate from '../screens/Donate';
-import { ITEM_WIDTH } from '../components/CarouselCardItem';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
@@ -11,14 +9,5 @@ describe('<Donate />', () => {
         const tree = renderer.create(<Donate />).toJSON();
         expect(tree).toMatchSnapshot();
     });
-/*
-describe('<AboutUs />', () => {
-    test("it should render correctly", () => {
-        const tree = renderer
-            .create(<Link page="https://physicaldiagnosispdx.com/card-tutorial/">Cariology</Link>)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-});
-*/
+
 });
