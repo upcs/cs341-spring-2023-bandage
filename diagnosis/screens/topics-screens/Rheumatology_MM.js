70940c0b684bbbe7489e05e46b10aee29062e0c1
santosty25 <santosty25@up.edu>
2023-03-02 11:49:51 -0800

    Rheumatology and Pulmonology Background
    Changed color for background

--- diagnosis/screens/topics-screens/Rheumatology_MM.js
+++ diagnosis/screens/topics-screens/Rheumatology_MM.js
@@ -2,14 +2,32 @@ import React from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const Rheumatology_MM = () => (
+const Rheumatology_MM = () => {
+    const handleShare = () => {
+        Share.share({
+          message: 'Check out this rheumatology multimedia content: https://up.physicaldiagnosispdx.com/up/app-content/server-screens/Rheumatology_MM.php',
+        });
+      };
+      const injectedJavaScript = `
+      var style = document.createElement('style');
+      style.innerHTML = 'body { font-family: sans-serif; }';
+      document.body.style.backgroundColor = '14CABA';
+      document.head.appendChild(style);
+    `; // THE JS THAT INJECTS
+    return (
     <View style={styles.mainContainer}>
         <WebView 
-            javaScriptEnabled={true} source={{uri: 'https://up.physicaldiagnosispdx.com/up/app-content/server-screens/Rheumatology_MM.php'}} 
+            javaScriptEnabled={true} 
+            source={{uri: 'https://up.physicaldiagnosispdx.com/up/app-content/server-screens/Rheumatology_MM.php'}} 
             style={styles.view}
+            injectedJavaScript={injectedJavaScript} // INJECT JS
         />
+        <TouchableOpacity style={styles.button} onPress={handleShare}>
+          <Text style={styles.buttonText}>Share</Text>
+        </TouchableOpacity>
     </View>
-);
+   );
+};
 
 const styles = StyleSheet.create({
     mainContainer: {
